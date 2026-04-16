491ccaf2901045c5d0adc81a9f221bd34150cc44
shettar2025 <shettar1278@gmail.com>
2026-04-16 23:08:07 +0530

    Update constant.groovy

--- vars/constant.groovy
+++ vars/constant.groovy
@@ -4,7 +4,7 @@ def call(appName) {
     def AWS_REGION = 'eu-north-1'
     def ECR_REPO   = appName
     def GIT_URL    = "https://github.com/shettar2025/${appName}.git"
-    def ECR_URL = "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com""
+    def ECR_URL = "${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
  def IMAGE_NAME = appName
     return [
         ACCOUNT_ID: ACCOUNT_ID,
